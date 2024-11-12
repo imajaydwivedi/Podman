d48b67d58af37bc1764333982fc9a3d52d5c0a2a
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2024-11-12 18:18:09 +0530

    added few scripts

---
+++ Help/podman-help.sh
@@ -0,0 +1,2 @@
+# Youtube - https://www.youtube.com/watch?v=YXfA5O5Mr18&list=PLFUGPe1byxeu5XQG_vKRVD7goLbsWM2zU&index=8
+
---
+++ Setup/configurations.sh
@@ -0,0 +1,8 @@
+
+# Custom registries
+$HOME/.config/containers/registries.conf
+
+[registries.search]
+registries = ['docker.io','ghrc.io','quay.io','registry.redhat.io']
+
+podman search apache --limit 5
